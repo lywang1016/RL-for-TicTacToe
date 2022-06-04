c094adc1e8fdfafc6fecab7d6f537608de89f80b
lywang1016 <lywang1016@hotmail.com>
2022-06-04 02:04:55 -0700

    test

--- collect_data.py
+++ collect_data.py
@@ -5,11 +5,11 @@ def main():
     dataset = h5py_to_dataset('dataset/x.hdf5', 'dataset/y.hdf5')
 
     # game = Game(r_type='human', b_type='human', if_record=False, if_dataset=True, ai_explore_rate=0)
-    game = Game(r_type='ai', b_type='ai', if_record=False, if_dataset=True, if_gui=False, gui_update=1, ai_explore_rate=0.05)
+    game = Game(r_type='ai', b_type='ai', if_record=False, if_dataset=True, if_gui=False, gui_update=1, ai_explore_rate=1)
     # game = Game(r_type='human', b_type='ai', if_record=False, if_dataset=True, ai_explore_rate=0.05)
     # game = Game(r_type='ai', b_type='human', if_record=False, if_dataset=True, ai_explore_rate=0.05)
 
-    for i in range(5):
+    for i in range(5000):
         game.episode()
         merge_dataset(dataset, game.chess_board.dataset)
 
