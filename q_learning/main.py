0aecd8e6eab9657ae342d5cff83f8352abb01bea
lywang1989 <lywang1016@hotmail.com>
2023-03-21 20:26:21 +0800

    reset game

--- q_learning/main.py
+++ q_learning/main.py
@@ -2,8 +2,8 @@ from game import HumanHumanGame, HumanAIGame, AIAIGame
 
 def main():
     # game = HumanHumanGame()
-    # game = HumanAIGame(ai_type='r')
-    game = AIAIGame(if_gui=False)
+    game = HumanAIGame(ai_type='b')
+    # game = AIAIGame(if_gui=False)
 
     for i in range(5):
         game.episode()
