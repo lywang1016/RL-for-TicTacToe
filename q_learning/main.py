588d0cb9bc1bf799a2f6a6e41954c4a57f1ef7c5
lywang1989 <lywang1016@hotmail.com>
2023-03-21 20:23:19 +0800

    basic game framwork

--- q_learning/main.py
+++ q_learning/main.py
@@ -1,7 +1,9 @@
-from game import HumanHumanGame
+from game import HumanHumanGame, HumanAIGame, AIAIGame
 
 def main():
-    game = HumanHumanGame()
+    # game = HumanHumanGame()
+    # game = HumanAIGame(ai_type='r')
+    game = AIAIGame(if_gui=False)
 
     for i in range(5):
         game.episode()
