3f4ca367d75e3c54e90312b832b18a041e71c360
lywang1989 <lywang1016@hotmail.com>
2023-03-23 15:57:06 +0800

    warp up

--- q_learning/utils.py
+++ q_learning/utils.py
@@ -22,54 +22,6 @@ def board_rotate_180lr(board_in):
     board = board_rotate_180(board_in)
     return board_rotate_lr(board)
 
-# def board_rotate_90(board_in):
-#     board = np.zeros((3, 3))
-#     for i in range(3):
-#         board[:,i] = board_in[2-i,:]
-#     return board
-
-# def board_rotate_270(board_in):
-#     board = np.zeros((3, 3))
-#     for i in range(3):
-#         board[i,:] = board_in[:,2-i]
-#     return board
-
-# def board_rotate_ud(board_in):
-#     board = np.zeros((3, 3))
-#     board[0,:] = board_in[2,:]
-#     board[1,:] = board_in[1,:]
-#     board[2,:] = board_in[0,:]
-#     return board
-
-# def board_trans(board):
-#     res = np.zeros((3,3))
-#     for i in range(3):
-#         for j in range(3):
-#             if board[i][j] >= 0:
-#                 res[i][j] = board[i][j]
-#             else:
-#                 res[i][j] = 2
-#     return res
-
-# def key_to_board(key):
-#     board = np.zeros((3, 3))
-#     for i in range(3):
-#         for j in range(3):
-#             board[i][j] = key[i][j]
-#     return board
-
-# def rotate_action(posi, value):
-#     value_ = -value
-#     posi_ = (2 - posi[0], 2 - posi[1])
-#     return posi_, value_
-
-# def board_turn180(board_in):
-#     board = board_in[::-1,::-1]
-#     for i in range(3):
-#         for j in range(3):
-#             board[i][j] = -board[i][j]
-#     return board
-
 if __name__ == '__main__':
     board = np.zeros((3, 3))
     board[0][0] = 1
